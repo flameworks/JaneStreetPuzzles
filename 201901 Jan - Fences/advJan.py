234e809c15ff83dfa45e3c97c1e06beaa862d06a
Dewei Chuan <8252966+flameworks@users.noreply.github.com>
2019-10-01 13:50:03 +0800

    Update advJan.py

--- 201901 Jan - Fences/advJan.py
+++ 201901 Jan - Fences/advJan.py
@@ -1,23 +1,46 @@
 import time
-import numpy as np
 from copy import deepcopy as dc, copy
-from heapq import heappush as hpush, heappop as hpop, heapify
-from basicJan import getGrid, getGrps, printer, counter, scorer, verifyGrid, getNeigh, getDiagNeigh
+from basicJan import getGrid, printer, counter, getNeigh, getDiagNeigh
 
 def main():
     g = getGrid()
-    flag = True
-    borders = []
-    while flag:
-        flag = False
+    prevCount = 0
+    while prevCount < counter(g):
+        prevCount = counter(g)
+        borders = []
         for i in range(17):
             for j in range(17):
                 if i == 0 or i == 16 or j == 0 or j == 16: borders.append((i,j))
                 pole = g[i][j]
                 if pole <= 0 or pole == 99: continue
-                flag, g = getBlockedGrid(g,i,j,flag)
+                getBlockedGrid(g,i,j)
         getDeadEnds(g)
         getBottleNeck(g,borders)
+        for i in range(17):
+            for j in range(17):
+                emptySpace = g[i][j]
+                if emptySpace != 0: continue
+                dcG = dc(g)
+                dcG[i][j] = 99
+                prevCountDC = 0
+                flagBreak = True
+                while prevCountDC < counter(dcG):
+                    prevCountDC = counter(dcG)
+                    borders2 = []
+                    for i2 in range(17):
+                        if not flagBreak: break
+                        for j2 in range(17):
+                            if i2 == 0 or i2 == 16 or j2 == 0 or j2 == 16: borders2.append((i2,j2))
+                            pole = dcG[i2][j2]
+                            if pole <= 0 or pole == 99: continue
+                            tempGDC = getBlockedGrid(dcG,i2,j2)
+                            if tempGDC == None: 
+                                flagBreak = False
+                                break
+                    if not flagBreak: break
+#                    getDeadEnds(dcG)
+                    getBottleNeck(dcG,borders2)  
+                if not flagBreak: g[i][j] = -99
     print(counter(g))
     return g
 
@@ -28,7 +51,7 @@ def getBottleNeck(g,borders):
     route2 = getWrap(g,islands)       
     finalRoute = list(set(route) & set(route2)) 
     for r1,r2 in finalRoute:
-        g[r1][r2] = 99
+        if g[r1][r2] == 0: g[r1][r2] = 99
     return g
 
 def getIslands(g,islands):
@@ -70,34 +93,22 @@ def getDeadEnds(g):
                 if ans <= 1: g[i][j] = -99
     return g
 
-def getBlockedGrid(g,i,j,flag):
-    pole = g[i][j]
+def getBlockedGrid(g,i,j):
+    pole = g[i][j]       
     topB,rhsB,botB,lhsB = get4d(g,(i,j))
-    if i-topB < 0: print("ERROR@@@@@@")
-    for idx in range(i-topB,i):
+    if i-topB < 0 or i+botB > 16: return None
+    if j-lhsB < 0 or j+rhsB > 16: return None    
+    for idx in range(i-topB,i+1+botB):
+        if idx == i: continue
         if g[idx][j] == -pole: continue
-        if g[idx][j] != 0 and g[idx][j] != -99: print("GG@@@@@")
+        if g[idx][j] != 0 and g[idx][j] != -99: return None
         g[idx][j] = -pole
-        flag = True
-    if i+botB > 16: print("ERROR@@@@@@")
-    for idx in range(i+1,i+1+botB):
-        if g[idx][j] == -pole: continue
-        if g[idx][j] != 0 and g[idx][j] != -99: print("GG@@@@@@")
-        g[idx][j] = -pole
-        flag = True
-    if j-lhsB < 0: print("ERROR@@@@@")
-    for idx in range(j-lhsB,j):
+    for idx in range(j-lhsB,j+1+rhsB):
+        if idx == j: continue
         if g[i][idx] == -pole: continue
-        if g[i][idx] != 0 and g[i][idx] != -99: print("GG@@@@@@")
+        if g[i][idx] != 0 and g[i][idx] != -99: return None
         g[i][idx] = -pole
-        flag = True              
-    if j+rhsB > 16: print("ERROR@@@@@@")
-    for idx in range(j+1,j+1+rhsB):
-        if g[i][idx] == -pole: continue
-        if g[i][idx] != 0 and g[i][idx] != -99: print("GG@@@@@@")
-        g[i][idx] = -pole
-        flag = True        
-    return [flag,g]
+    return g
 
 def get4d(grid,coord):
     r,c = coord; num = grid[r][c]
@@ -126,3 +137,21 @@ start = time.time()
 printer(main())
 end = time.time()
 print("Time Taken: ",end - start)    
+
+'''
+A 17-by-17 field is divided into unit squares. Some of the squares contain “posts” 
+at their center. Each post is represented below by a number. Construct one or more
+ fences emanating from each post, such that the total length of fence connected to 
+ a post equals the number given. Fences have integer length and can only be constructed
+ horizontally or vertically. Fences from different posts may not touch, nor may 
+ a fence from one post touch a different post.
+
+The goal of this puzzle is to build your fences in such a manner that it is possible
+ to draw a closed loop through some of the remaining empty squares. The loop must 
+ enclose at least one post, and must be symmetric in some way (either via
+ rotation or reflection). As in the example, the loop must be rectilinear, 
+ passing through the centers of adjacent empty squares.
+
+The answer to this puzzle is the product of the fence-lengths of the fences inside
+ the loop. (Note that in the Example, the answer is 2, not 4.
+'''
